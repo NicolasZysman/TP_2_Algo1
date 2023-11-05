b671406d64bb58d00f4fc5dd8f537e1e9cf465e0
NicolasZysman <nzysman@gmail.com>
2023-11-05 12:10:56 -0300

    tkinter basico

---
+++ cartelera.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+# import os
+# import requests
+# import qrcode
+
+ventana = tk.Tk(className = "Cartelera")
+ventana.geometry("500x500")
+
+ubicacion = tk.Label(ventana, text = "Palermo, Belgrano, Villa Crespo")
+ubicacion.pack()
+
+
+
+ventana.mainloop()
