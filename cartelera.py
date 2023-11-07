9412fe7568500039b1d7e349935797a6caa39440
NicolasZysman <nzysman@gmail.com>
2023-11-07 11:32:27 -0300

    endpoint cinemas y main

--- cartelera.py
+++ cartelera.py
@@ -6,18 +6,33 @@ import requests
 
 API_KEY: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.DGI_v9bwNm_kSrC-CQSb3dBFzxOlrtBDHcEGXvCFqgU"
 
-Headers = { "Authorization" : API_KEY }
+def autorizacion() -> dict:
+    
+    Headers: dict = { "Authorization" : f"Bearer {API_KEY}" }
 
-r = requests.get("https://cinemas", headers=Headers)
-r_json = r.json()
+    return Headers
+
+def get_cinemas(Headers: dict) -> list[dict]:
+
+    r_ubicacion = requests.get("http://vps-3701198-x.dattaweb.com:4000/cinemas", headers=Headers)
+    info_cinemas: list[dict] = r_ubicacion.json()
+
+    return info_cinemas
+
+def main() -> None:
+    
+    Headers = autorizacion()
+
+    info_cinemas = get_cinemas(Headers)
+
+main()
 
 
-ventana = tk.Tk(className = "Cartelera")
-ventana.geometry("500x500")
+# ventana = tk.Tk(className = "Cartelera")
+# ventana.geometry("500x500")
 
-ubicacion = tk.Label(ventana, text = "Palermo, Belgrano, Villa Crespo")
-ubicacion.pack()
+# ubicacion = tk.Label(ventana, text = "Palermo, Belgrano, Villa Crespo")
+# ubicacion.pack()
 
-print("prueba rama")
 
-ventana.mainloop()
+# ventana.mainloop()
