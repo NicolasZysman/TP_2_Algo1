69ac477be4e9e0f09432980870789cbc8e5245f9
NicolasZysman <nzysman@gmail.com>
2023-11-13 16:33:19 -0300

    comente funcion que rompe

--- cartelera.py
+++ cartelera.py
@@ -161,15 +161,16 @@ def botones_ubicacion(ventana_ubicaciones, ubicaciones: list) -> None:
         boton = tk.Button(ventana_ubicaciones, text=ubicacion, command=lambda e= i+1: onbutton_click(e))
         boton.grid(row=0, column=i)
 
-def mostrar_posters(lista_posters: list, ventana_ubicaciones) -> None:
+# def mostrar_posters(lista_posters: list, ventana_ubicaciones) -> None:
     
-    for poster in lista_posters:
-        # print(poster)
-        # img = Image.open(poster)
-        # tk_img = ImageTk.PhotoImage(img)
-        img = ImageTk.PhotoImage(data=poster)
-        boton = tk.Button(ventana_ubicaciones, image=img)
-        boton.grid(row=0, column=2)
+#     for poster in lista_posters:
+#         # print(poster)
+#         # img = Image.open(poster)
+#         # tk_img = ImageTk.PhotoImage(img)
+#         img = cv2.imread(poster, -1)
+#         cv2.imshow("Image", img)
+#         # boton = tk.Label(ventana_ubicaciones, image=img)
+#         # boton.grid(row=3, column=2)
         
 
 def main() -> None:
@@ -189,7 +190,7 @@ def main() -> None:
     ubicaciones = obtener_ubicaciones(info_cines)
     ventana_ubicaciones = crear_ventana_ubicaciones()
     botones_ubicacion(ventana_ubicaciones, ubicaciones)
-    mostrar_posters(lista_posters, ventana_ubicaciones)
+    # mostrar_posters(lista_posters, ventana_ubicaciones)
     ventana_ubicaciones.mainloop()
     # info_pelis_en_cine = get_pelis_en_cine(Headers, cine_id)
     # lista_pelis_en_cine: list = [diccionario["has_movies"] for diccionario in info_pelis_en_cine]
